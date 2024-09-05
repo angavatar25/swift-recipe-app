3c645999a65f8ba07a04fb8f515e1db956b0324f
Ananda Afryan Gemilang <anandalark007@gmail.com>
2024-09-05 10:10:51 +0700

    Initial Commit

---
+++ dogFacts/dogFactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  dogFactsApp.swift
+//  dogFacts
+//
+//  Created by Ananda Afryan on 05/09/24.
+//
+
+import SwiftUI
+
+@main
+struct dogFactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
